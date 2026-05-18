0b2c93fe308df21c5cc0cb837aa9cd90d3acdc3d
zzaain <sheriellaghzain@gmail.com>
2026-05-18 11:00:10 +0700

    membuat class baseclass

--- nopewarisan.cpp
+++ nopewarisan.cpp
@@ -7,24 +7,3 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
-
-class baseClass1 {
-public:
-    virtual void perkenalan() {
-        cout << "Hallo saya Function dari base class";
-    }
-};
-
-class derivedClass : public baseClass1 {
-public:
-    void perkenalan() {
-        cout << "Hallo saya Function dari derived Class";
-    }
-};
-
-int main() {
-    derivedClass a;
-    a.perkenalan();
-
-    return 0;
-}

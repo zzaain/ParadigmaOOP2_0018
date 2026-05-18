855740d6827b4ed7c9da5a7f3f005c55e26319ad
zzaain <sheriellaghzain@gmail.com>
2026-05-18 10:54:26 +0700

    nambahin fungsi set data

---
+++ abstraksi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+};
+

c5a17e836e8244b0e4c64f09f94c45b9b01ed8ea
zzaain <sheriellaghzain@gmail.com>
2026-05-18 10:22:42 +0700

    menambahkan int main dan pemanggilan obyek

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -22,3 +22,17 @@ public:
         cout << "Pesan dari lia" << endl;
     }
 };
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+    //b.seseorang::pesan();
+    return 0;
+}

4922a394bcc0eb4bd0f3e1fc97a03332e862d433
zzaain <sheriellaghzain@gmail.com>
2026-05-18 10:21:00 +0700

    menambahkan class seseorang

---
+++ polimorpi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+    // virtual void pesan(){
+    //      cout<<"Pesan dari seseorang"<<endl;
+    // }
+};
+

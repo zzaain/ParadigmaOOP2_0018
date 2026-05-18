966308bf5285bdea4a78e0b30527ef04918720bd
zzaain <sheriellaghzain@gmail.com>
2026-05-18 10:55:17 +0700

    nambahin fungsi display

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -12,5 +12,12 @@ public:
         x = a;
         y = b;
     }
+
+     //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
 };
+
 
